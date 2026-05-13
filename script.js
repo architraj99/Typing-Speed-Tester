bf78eee978858b7db31020ffa0f1c1b06a6af253
architraj99 <rajarchit99@gmail.com>
2026-05-13 20:57:14 +0530

    Made typing tester layout
    Created the base layout for the typing speed tester with sample text area...

---
+++ script.js
@@ -0,0 +1,23 @@
+let textBox = document.getElementById("textBox");
+let typingInput = document.getElementById("typingInput");
+
+let startBtn = document.getElementById("startBtn");
+let restartBtn = document.getElementById("restartBtn");
+
+let testStatus = document.getElementById("testStatus");
+
+let timeValue = document.getElementById("timeValue");
+let wpmValue = document.getElementById("wpmValue");
+let accuracyValue = document.getElementById("accuracyValue");
+let mistakeValue = document.getElementById("mistakeValue");
+
+let resultBox = document.getElementById("resultBox");
+
+startBtn.onclick = function() {
+
+};
+
+restartBtn.onclick = function() {
+
+};
+
